88048016cc73d9ee9bda0e58b8746fcbd229c5aa
Marasy Phi <hipporasy@gmail.com>
2023-01-11 15:37:06 +1100

    Initial Commit

---
+++ HippoStore/HippoStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HippoStoreApp.swift
+//  HippoStore
+//
+//  Created by Marasy Phi on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HippoStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
